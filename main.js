52af0724fc7fbabc0a013bbb911356765de5a243
Dylan Conway <Dylan>
2018-12-16 16:12:27 -0800

    js and css directories

---
+++ main.js
@@ -0,0 +1,31 @@
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow();
+    win.setFullScreen(true);
+
+    win.loadFile('main.html');
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if(win === null){
+        createWindow();
+    }
+})
